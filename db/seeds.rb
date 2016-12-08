6697440e78894068b4a810c914962f362d6e5a6a
Eitan <eitanakman@gmail.com>
2016-12-08 11:52:29 -0500

    Attempt to fix seeding issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.new({username: 'user1', password: 'password'})
+User.create({username: 'user2', password: 'password'})
