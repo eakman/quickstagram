020947a8c0d54de8a8289e8f89898193b8dd18ec
Eitan <eitanakman@gmail.com>
2016-12-13 10:18:47 -0500

    Make comment username link to their profile page

--- frontend/components/comment.jsx
+++ frontend/components/comment.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import { Link }  from 'react-router';
 
-const Comment = ({ comment }) => {
-  // debugger
-  return (
-    //jshint ignore: start
-    <div className='comment-body' key={'cmnt-body-' + comment.id}>
-      <div className='comment-text' key={'cmnt-text-' + comment.id}>
-          <strong className='comment-uname' key={'cmnt-uname-' + comment.id}>{ comment.user.username }</strong> { comment.body }
+class Comment extends React.Component {
+  constructor(props) {
+    super(props);
+    // debugger
+
+    this.showUser = this.showUser.bind(this);
+  }
+  showUser(e) {
+
+  }
+  render() {
+    const { comment } = this.props;
+    const userUrl = `/main/profile/${comment.user.id}`;
+    return (
+      //jshint ignore: start
+      <div className='comment-body' key={'cmnt-body-' + comment.id}>
+        <div className='comment-text' key={'cmnt-text-' + comment.id}>
+            <Link to={ userUrl } className='comment-uname' key={'cmnt-uname-' + this.props.id}>
+              { comment.user.username }
+            </Link> { comment.body }
+        </div>
       </div>
-    </div>
-    //jshint ignore: end
-  );
-};
+      //jshint ignore: end
+    );
+  }
+}
 
 export default Comment;
