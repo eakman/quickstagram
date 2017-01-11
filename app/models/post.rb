dc59a166b8d3ed7969f595aff17ff22d0f1b4dc3
Eitan <eitanakman@gmail.com>
2017-01-10 22:02:44 -0500

    Image compression and resizing

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,8 +17,10 @@ require "open-uri"
 
 class Post < ActiveRecord::Base
   validates :picture_url, :user_id, presence: true
-
-  has_attached_file :picture_url, default_url: "instagram-chicagofoodauthority.jpg", processors: [:compression]
+#
+  has_attached_file :picture_url, default_url: "instagram-chicagofoodauthority.jpg",
+      styles: { medium: "618x500#" }, convert_options: { medium: "-quality 40" },
+       processors: [:thumbnail, :compression]
   validates_attachment_content_type :picture_url, content_type: /\Aimage\/.*\z/
 
   def picture_from_url(url)
